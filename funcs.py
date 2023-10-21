b9344339c0113874b9d23dbb463ef47bd118375d
joachim.lehmann@googlemail.com <joachim.lehmann@googlemail.com>
2023-10-22 00:48:41 +0200

    cleaned up.

--- funcs.py
+++ funcs.py
@@ -3,15 +3,6 @@ import telebot
 import cloudscraper
 
 
-def getstorelist(zipcode):
-    urlstring = f"https://www.rewe.de/api/marketsearch?searchTerm={zipcode}"
-    client = ZenRowsClient(API_KEY)
-
-    response = client.get(urlstring)
-    # print(response.json())
-    return response.json()
-
-
 def getstorelistcs(zipcode):
     url = f"https://www.rewe.de/api/marketsearch?searchTerm={zipcode}"
     scraper = cloudscraper.create_scraper(delay=10, browser="chrome")
