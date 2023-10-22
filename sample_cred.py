226e694fa4a9f0152a8c7aebbde530e061f2b487
huskycgn <92435764+huskycgn@users.noreply.github.com>
2023-10-22 02:10:58 +0200

    Create sample_cred.py
    Created sample cred file.

---
+++ sample_cred.py
@@ -0,0 +1,9 @@
+TELE_TOKEN = "YourTelegramBotToken"
+# Create in Telegram with Botfather
+TELE_CHAT_ID = "Your_Telegram_group_chat_ID"
+# Create a group and invite your bot - fire up telegram in your browser
+# go to that group and copy the chat id from the browsers address field. 
+# Don't forget the leading "-"!
+PATH = "./"
+# Path to listfile with desired items.
+# One each line.
